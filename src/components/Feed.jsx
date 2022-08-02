86d75477425edf012371719c418123c178ea8943
Siegfried <siegfried.bozza@renuo.ch>
2022-08-02 15:17:18 +0200

    refactor

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { PlasmicFeed } from "./plasmic/copy_of_minitwitter/PlasmicFeed";
-import Post from './Post'
-import moment from "moment"
+import Post from './Post';
+import moment from 'moment';
 
 function Feed_({posts, ...rest}, ref) {
- 
+
   return <PlasmicFeed
     root={{ ref }} {...rest}
     postList={{
-      children: posts.map(post => <Post timestamp={moment(post.createdAt).fromNow()}>{post.title}</Post>),
+      children: posts.map(post => <Post timestamp={moment(post.createdAt).fromNow()}>{post.content}</Post>),
     }}
     />;
 }
