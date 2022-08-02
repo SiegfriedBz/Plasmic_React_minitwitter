69bcac3487856789d700381f1658bf3fb7193427
Siegfried <siegfried.bozza@renuo.ch>
2022-08-02 10:41:12 +0200

    add react router + wire  plasmic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useState } from "react"
+import { Routes, Route } from "react-router-dom"
 import Feed from './components/Feed';
+import NewPost from "./components/NewPost"
+import { v4 } from 'uuid';
+
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {id: 1, title: "Hello world 01", time: Math.floor(Math.random() * 24)},
+    {id: 2, title: "Hello world 02", time: Math.floor(Math.random() * 24)},
+    {id: 3, title: "Hello world 03", time: Math.floor(Math.random() * 24)},
+  ]);
+
+  const onAddPost = (postContent) => {
+    setPosts([...posts, {title: postContent, time: Math.floor(Math.random() * 24), id: v4()}])
+  }
+
   return (
     <div>
-      <Feed />
+      <Routes>
+        <Route path="/" element={<Feed posts={posts}/>} />
+        <Route path="/post" element={<NewPost onAddPost={onAddPost} />} />
+      </Routes>
     </div>
   );
 }
